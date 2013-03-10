abbcb257eb3b4609e789d1489e75fdb61ba56813
JeanFred <JeanFred@github>
2013-03-10 22:26:53 +0100

    Add MetadataMapping feature
    We want to be able to map metadata to a wiki-based alignment.
    This is done as a PostProcessing method.

--- uploadlibrary/PostProcessing.py
+++ uploadlibrary/PostProcessing.py
@@ -36,3 +36,26 @@ def process_DIMS(field, old_field_value):
     if len(splitted[2::3]) > 0:
         DIMS_BIS["_".join([field, 'unit'])] = splitted[2::3][0]
     return DIMS_BIS
+
+
+class MetadataMapping():
+
+    """Mapping metadata using aligment."""
+
+    def __init__(self, mapper=None):
+        self.mapper = mapper
+
+    def process_with_alignment(self, field, old_field_value):
+        """Retrieve the alignment for a given record contents."""
+        if self.mapper is None:
+            return
+        else:
+            new_value = dict()
+            (value, categories) = self.mapper[field].get(content, ("", []))
+            if value:
+                new_value[field] = value
+            else:
+                new_value[field] = old_field_value
+            if categories:
+                new_value[categories] = categories
+            return new_value
