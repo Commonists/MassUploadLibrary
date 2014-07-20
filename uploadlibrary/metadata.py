16838eea14f6ef449727b8c6c98ab392d6daa34d
Jean-Fred <JeanFred@github>
2014-07-20 12:45:45 +0200

    Return string instead of printing directly categorisation statistics
    We do not always want this to be printed around.

--- uploadlibrary/metadata.py
+++ uploadlibrary/metadata.py
@@ -278,30 +278,37 @@ def categorisation_statistics(all_categories, categories_count_per_file):
         import numpy
     except ImportError:
         numpy_available = False
-
-    print "= Categoriation statistics ="
-    print "== Per category =="
-    print "The program added {0:d} categories, {1:d} distinct ones".format(sum(all_categories.values()),
-                                                                           len(all_categories))
-    print "The most used category is on {0:d} files".format(max(all_categories.values()))
-    print "The less used on {0:d} files".format(min(all_categories.values()))
+    text = list()
+    text.append("= Categoriation statistics =")
+    text.append("== Per category ==")
+    text.append("The collection has {0:d} categories, {1:d} distinct ones".format(sum(all_categories.values()),
+                                                                                  len(all_categories)))
+    try:
+        text.append("The most used category is on {0:d} files".format(max(all_categories.values())))
+        text.append("The less used on {0:d} files".format(min(all_categories.values())))
+    except ValueError:
+        pass
     if numpy_available:
-        print "On average, a category is used {0:.1f} times (mean)".format(numpy.mean(all_categories.values()))
-        print "The median is: {0:.1f}".format(numpy.median(all_categories.values()))
-    print "The 10 most used categories are:"
-    print all_categories.most_common(10)
-    print "The 10 less used categories are:"
-    print all_categories.most_common()[-10:]
+        text.append("On average, a category is used {0:.1f} times (mean)".format(numpy.mean(all_categories.values())))
+        text.append("The median is: {0:.1f}".format(numpy.median(all_categories.values())))
+    text.append("The 10 most used categories are:")
+    text.append(" - ".join([unicode(x[0]) + ' - ' + unicode(x[1])
+                            for x in all_categories.most_common(10)]))
+    text.append("The 10 less used categories are:")
+    text.append(" - ".join([unicode(x[0]) + ' - ' + unicode(x[1])
+                            for x in all_categories.most_common()[-10:]]))
 
-    print "== Per file =="
-    print "The most categorized file has {0:d} categories".format(max(categories_count_per_file.values()))
-    print "The less categorized file has {0:d} categories".format(min(categories_count_per_file.values()))
-    print "We have {0:d} uncategorized files".format(len([x for x in categories_count_per_file
-                                                          if categories_count_per_file[x] is 0]))
-    more_than_two = len([x for x in categories_count_per_file if categories_count_per_file[x] >= 2])
+    text.append("== Per file ==")
+    text.append("The most categorized file has {0:d} categories".format(max(categories_count_per_file.values())))
+    text.append("The less categorized file has {0:d} categories".format(min(categories_count_per_file.values())))
+    text.append("We have {0:d} uncategorized files".format(len([x for x in categories_count_per_file
+                                                           if categories_count_per_file[x] is 0])))
+    more_than_two = len([x for x in categories_count_per_file
+                         if categories_count_per_file[x] >= 2])
     more_than_two_percentage = float(more_than_two) / len(categories_count_per_file) * 100
-    print "We have {0:d} files with two categories or more, which makes {1:.1f}%".format(more_than_two,
-                                                                                         more_than_two_percentage)
+    text.append("We have {0:d} files with two categories or more, which makes {1:.1f}%".format(more_than_two,
+                                                                                               more_than_two_percentage))
     if numpy_available:
-        print "On average, a file has {0:.1f} categories (mean)".format(numpy.mean(categories_count_per_file.values()))
-        print "The median is: {0:.1f}".format(numpy.median(categories_count_per_file.values()))
+        text.append("On average, a file has {0:.1f} categories (mean)".format(numpy.mean(categories_count_per_file.values())))
+        text.append("The median is: {0:.1f}".format(numpy.median(categories_count_per_file.values())))
+    return '\n'.join(text)
